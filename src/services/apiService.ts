3879c8000a93b5f2a5ae850371233344e6b4e9e0
Matthew Ham <hammatthew70@gmail.com>
2025-06-17 23:28:27 -0400

    setup for weather api call

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -8,12 +8,18 @@ export class ApiService {
     private static geocodeAPIKey: string = process.env.GEOCODE_API_KEY || ''
 
     static async get_location_data(location: string){
-        console.log('hello we are about to fetch some data from the google api')
         const resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${this.geocodeAPIKey}`)
 
-        if (!resp.ok){
-            throw new Error(`Failed with status ${resp.status}`);
-        }
+        if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
+
+        const data = await resp.json()
+        return data
+    }
+
+    static async get_weather_data(lat: number, lng: number){
+        const resp = await fetch(``)
+
+        if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
 
         const data = await resp.json()
         return data
