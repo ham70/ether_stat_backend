a3616dc533701500877e24d22c46249a504e8a40
Matthew Ham <hammatthew70@gmail.com>
2025-06-18 22:38:28 -0400

    fixed indentations

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,24 +4,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export class ApiService {
-    private static weatherAPIKey: string = process.env.WEATHER_API_KEY || ''
-    private static geocodeAPIKey: string = process.env.GEOCODE_API_KEY || ''
+  private static weatherAPIKey: string = process.env.WEATHER_API_KEY || ''
+  private static geocodeAPIKey: string = process.env.GEOCODE_API_KEY || ''
 
-    static async get_location_data(location: string){
-        const resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${this.geocodeAPIKey}`)
+  static async get_location_data(location: string) {
+    const resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${this.geocodeAPIKey}`)
 
-        if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
+    if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
 
-        const data = await resp.json()
-        return data
-    }
+    const data = await resp.json()
+    return data
+  }
 
-    static async get_weather_data(lat: number, lng: number){
-        const resp = await fetch(``)
+  static async get_weather_data(lat: number, lng: number) {
+    const resp = await fetch(``)
 
-        if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
+    if (!resp.ok) throw new Error(`Failed with status ${resp.status}`)
 
-        const data = await resp.json()
-        return data
-    }
+    const data = await resp.json()
+    return data
+  }
 }
