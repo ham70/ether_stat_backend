2fe8b2d0bdf8a7e36f2fca996488a5096d686fb9
Matthew Ham <hammatthew70@gmail.com>
2025-06-26 22:54:23 -0400

    updated routes to handle new api serives

--- src/routes/cityRoutes.ts
+++ src/routes/cityRoutes.ts
@@ -16,46 +16,18 @@ router.get('/', async (req, res) => {
   }
 
   //handle data
-  const geocode_data = await ApiService.get_location_data(query)
-  const geo_result = geocode_data.results[0]
+  const loc_data = await ApiService.get_location_data(query)
+  const weather_data: WeatherData = await ApiService.get_weather_data(loc_data.id, loc_data.lat, loc_data.lng)
+  const aqi_data = await ApiService.get_aqi_data(loc_data.id, loc_data.lat, loc_data.lng)
+  const demo_data = await ApiService.get_demo_data(loc_data.id, loc_data.fips_codes.county, loc_data.fips_codes.state)
 
-  const weather_data = await ApiService.get_weather_data(geo_result.geometry.location.lat, geo_result.geometry.location.lng)
-  const aqi_data = await ApiService.get_aqi_data(geo_result.geometry.location.lat, geo_result.geometry.location.lng)
-  
   return res.json({
-    city: {
-      id: geo_result.place_id,
-      name: query,
-      lat: geo_result.geometry.location.lat,
-      lng: geo_result.geometry.location.lng,
-      weather_data: {
-        location_id: geo_result.place_id,
-        conditions: weather_data.weatherCondition.description.text,
-        temperature: {
-          main: weather_data.temperature.degrees,
-          feels_like: weather_data.feelsLikeTemperature.degrees,
-          min: weather_data.currentConditionsHistory.maxTemperature.degrees,
-          max: weather_data.currentConditionsHistory.minTemperature.degrees
-        },
-        humidity: weather_data.relativeHumidity,
-        wind: {
-          direction: weather_data.wind.direction,
-          speed: weather_data.wind.speed.value,
-          gust: weather_data.wind.gust.value,
-          chill: weather_data.windChill.degrees,
-        },
-        thunder_storm: weather_data.thunderstormProbability,
-        visibility: weather_data.visibility.distance,
-        uv_index: weather_data.uvIndex,
-        created_at: 'today'
-      },
-      aqi_data: {
-        location_id: geo_result.place_id,
-        aqi: aqi_data.indexes[0].aqi,
-        category: aqi_data.indexes[0].category,
-        dom: aqi_data.indexes[0].dominantPollutant
-      }
-    }
+    id: loc_data.id,
+    name: loc_data.name,
+    location: loc_data,
+    weather: weather_data,
+    aqi_data: aqi_data,
+    demographics: demo_data
   })
 })
 
--- src/routes/refreshRoutes.ts
+++ src/routes/refreshRoutes.ts
@@ -16,38 +16,13 @@ router.post('/', async (req, res) => {
   }
 
   //handle data
-  const weather_data = await ApiService.get_weather_data(data.lat, data.lng)
-  const aqi_data = await ApiService.get_aqi_data(data.lat, data.lng)
+  const weather_data = await ApiService.get_weather_data(data.id, data.lat, data.lng)
+  const aqi_data = await ApiService.get_aqi_data(data.id, data.lat, data.lng)
 
   return res.json({
     id: data.id,
-    weather_data: {
-      location_id: data.id,
-      conditions: weather_data.weatherCondition.description.text,
-      temperature: {
-        main: weather_data.temperature.degrees,
-        feels_like: weather_data.feelsLikeTemperature.degrees,
-        min: weather_data.currentConditionsHistory.maxTemperature.degrees,
-        max: weather_data.currentConditionsHistory.minTemperature.degrees
-      },
-      humidity: weather_data.relativeHumidity,
-      wind: {
-        direction: weather_data.wind.direction,
-        speed: weather_data.wind.speed.value,
-        gust: weather_data.wind.gust.value,
-        chill: weather_data.windChill.degrees,
-      },
-      thunder_storm: weather_data.thunderstormProbability,
-      visibility: weather_data.visibility.distance,
-      uv_index: weather_data.uvIndex,
-      created_at: 'today'
-    },
-    aqi_data: {
-        location_id: data.id,
-        aqi: aqi_data.indexes[0].aqi,
-        category: aqi_data.indexes[0].category,
-        dom: aqi_data.indexes[0].dominantPollutant
-    }
+    weather_data: weather_data,
+    aqi_data: aqi_data
   })
 })
 
