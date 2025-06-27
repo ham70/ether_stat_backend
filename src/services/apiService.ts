706dc7d901f944df6acae38a50b7f6b122b32301
Matthew Ham <hammatthew70@gmail.com>
2025-06-26 21:40:45 -0400

    updated det_location_data service to now retrieve fips code data and county name and return a proper LocationData object

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,4 +1,4 @@
-import { WeatherData, DemographicData } from '../types'
+import { LocationData, WeatherData, AirQualityData, DemographicData } from '../types'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -8,13 +8,33 @@ export class ApiService {
   private static geocodeAPIKey: string = process.env.GEOCODE_API_KEY || ''
   private static aqiAPIKey: string = process.env.AQI_API_KEY || ''
 
-  static async get_location_data(location: string) {
-    const resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${this.geocodeAPIKey}`)
+  static async get_location_data(location: string): Promise<LocationData>{
+    const geo_resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${this.geocodeAPIKey}`)
 
-    if (!resp.ok) throw new Error(`geocode api callfailed with status ${resp.status}`)
+    if (!geo_resp.ok) throw new Error(`geocode api callfailed with status ${geo_resp.status}`)
 
-    const data = await resp.json()
-    return data
+    const geo_data = await geo_resp.json()
+
+    const loc_resp = await fetch(`https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x=${geo_data.results[0].geometry.location.lat}&y=${geo_data.results[0].geometry.location.lng}&benchmark=Public_AR_Current&vintage=Current_Current&format=json`)
+    const loc_data = await loc_resp.json()
+
+    const current_date: Date = new Date()
+    const string_date: string = current_date.toString()
+    
+    const clean_data: LocationData = {
+      id: geo_data.results[0].place_id,
+      name: geo_data.results[0].address_components[0].long_name,
+      lat: geo_data.results[0].geometry.location.lat,
+      lng: geo_data.results[0].geometry.location.lng,
+      county_name: loc_data.result.geographies.Counties[0].NAME,
+      fips_codes: {
+        county: loc_data.result.geographies.Counties[0].COUNTY,
+        state: loc_data.result.geographies.Counties[0].STATE
+      },
+      created_at: string_date,
+      updated_at: string_date
+    }
+    return clean_data
   }
 
   static async get_weather_data(lat: number, lng: number) {
