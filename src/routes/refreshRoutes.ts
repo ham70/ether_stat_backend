a19b0c8a5c08b900170fbeb2b9801619f348c662
Matthew Ham <hammatthew70@gmail.com>
2025-07-01 22:09:42 -0400

    modifed refresh post route to now check the database to weather and air data to see how recent the data is before calling apis to get new data

--- src/routes/refreshRoutes.ts
+++ src/routes/refreshRoutes.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { CityDataResponse, WeatherData, RefreshDataResponse, RefreshDataRequest } from "../types"
 import { ApiService } from '../services/apiService'
+import Database from '../database/database'
 
 const router = express.Router()
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
+
+const db = new Database()
 
 //post routes
 router.post('/', async (req, res) => {
@@ -15,15 +18,33 @@ router.post('/', async (req, res) => {
     return res.status(400).json({ error: 'Missing data in post body' })
   }
 
-  //handle data
-  const weather_data = await ApiService.get_weather_data(data.id, data.lat, data.lng)
-  const aqi_data = await ApiService.get_aqi_data(data.id, data.lat, data.lng)
+  const weather = await db.getAirQualityData(data.id)
+  const air = await db.getWeatherData(data.id)
 
-  return res.json({
-    id: data.id,
-    weather_data: weather_data,
-    aqi_data: aqi_data
-  })
+  //checking weather data age
+  const date = new Date(weather.created_at)
+  const now = new Date()
+
+  const diffMs = now.getTime() - date.getTime();
+  const diffHours = diffMs / (1000 * 60 * 60);
+
+  if(diffHours < 1){
+    return res.json({
+      id: data.id,
+      weather_data: weather,
+      aqi_data: air
+    })
+  } else {
+    //time to grab new data
+    const weather_data = await ApiService.get_weather_data(data.id, data.lat, data.lng)
+    const aqi_data = await ApiService.get_aqi_data(data.id, data.lat, data.lng)
+
+    return res.json({
+      id: data.id,
+      weather_data: weather_data,
+      aqi_data: aqi_data
+    })
+  }
 })
 
 
